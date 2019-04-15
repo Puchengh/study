9244d7bb1cf7a6c750568bc3037f6c6f4e1d2209
puchen <740495380@qq.com>
2019-04-15 22:34:27 +0800

    update

--- src/main/java/com/puchen/linux/for.sh
+++ src/main/java/com/puchen/linux/for.sh
@@ -18,5 +18,15 @@ rm -rf ls.log
 read -p "Please input user name:" -t 30 name
 read -p "Please input the number of users:" -t 30 num
 read -p "Please input the password of users:" -t 30 pass
-if [ -z "$name" -a ! -z"$num" -a ! -z "$pass" ]
+if [ -z "$name" -a ! -z "$num" -a ! -z "$pass" ]
     then
+    y=$(echo $num|sed 's/[0-9]//g')
+        if [ -z "$y" ]
+            then
+                for(( i=1;i<=$num;i=i+1 ))
+                    do
+                        /usr/sbin/useradd $name$i &> /dev/null
+                            echo $pass|/usr/bin/passwd --stdin $name$i &>/dev/null
+                     done
+        fi
+fi
