a9338a709aa9f974551b137f1386264abbd473d0
Pu Chen <740495380@qq.com>
2020-06-06 23:28:40 +0800

    提交代码

--- src/main/java/com/puchen/work/test.java
+++ src/main/java/com/puchen/work/test.java
@@ -1,47 +1,48 @@
 package com.puchen.work;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+//import java.io.*;
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class test {
 
     public static void main(String args[]) {
 
-        String path1 = "D:\\1.txt";
-        String path2 = "D:\\2.txt";
-        List<String> list1 = readFile(path1);
-        List<String> list2 = readFile(path2);
-        for (int i=0;i< list1.size();i++ ){
-            System.out.println("update sys_res set app_unique = "+ "'" + list2.get(i) + "'" +
-                    " where res_name = " + "'" + list1.get(i) + "'" + ";");
-        }
+//        String path1 = "D:\\1.txt";
+//        String path2 = "D:\\2.txt";
+//        List<String> list1 = readFile(path1);
+//        List<String> list2 = readFile(path2);
+//        for (int i=0;i< list1.size();i++ ){
+//            System.out.println("update sys_res set app_unique = "+ "'" + list2.get(i) + "'" +
+//                    " where res_name = " + "'" + list1.get(i) + "'" + ";");
+//        }
+        System.out.println("5"+2);
 
     }
 
 
-    public static List<String> readFile(String str) {
-        List<String> list = new ArrayList<String>();
-        try {
-            File file = new File(str);
-            if (file.isFile() && file.exists()) {
-                InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "utf-8");
-                BufferedReader br = new BufferedReader(isr);
-                String lineTxt = null;
-                while ((lineTxt = br.readLine()) != null) {
-                    list.add(lineTxt);
-//                    System.out.println(lineTxt);
-                }
-                br.close();
-                return list;
-            } else {
-                System.out.println("文件不存在!");
-            }
-
-            return list;
-        } catch (Exception e) {
-            System.out.println("文件读取错误!");
-        }
-        return list;
-    }
+//    public static List<String> readFile(String str) {
+//        List<String> list = new ArrayList<String>();
+//        try {
+//            File file = new File(str);
+//            if (file.isFile() && file.exists()) {
+//                InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "utf-8");
+//                BufferedReader br = new BufferedReader(isr);
+//                String lineTxt = null;
+//                while ((lineTxt = br.readLine()) != null) {
+//                    list.add(lineTxt);
+////                    System.out.println(lineTxt);
+//                }
+//                br.close();
+//                return list;
+//            } else {
+//                System.out.println("文件不存在!");
+//            }
+//
+//            return list;
+//        } catch (Exception e) {
+//            System.out.println("文件读取错误!");
+//        }
+//        return list;
+//    }
 
 }
